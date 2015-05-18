35e04fc2ca6d0fa739fba7c7b3a46707dfd8b0d9
xululu <xululu@eventmosh.com>
2015-05-18 18:57:50 +0800

    Commit all

--- mediaplayer/php/CuPlayerSetFile.php
+++
@@ -1,9 +0,0 @@
-﻿<content>
-  <CuPlayerWidth>600</CuPlayerWidth>
-  <CuPlayerHeight>400</CuPlayerHeight>
-  <CuPlayerAutoPlay>yes</CuPlayerAutoPlay>    
-  <CuPlayerImage>images/start.jpg</CuPlayerImage>
-<CuPlayerLogo>images/Logo.png</CuPlayerLogo>
-<CuPlayerPosition>top-right</CuPlayerPosition>
-  <CuPlayerFile>test.mp4</CuPlayerFile>
-</content>

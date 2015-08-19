ea53981a461242039f06a39f7d5c6db2f17b42a1
xululu <xuyanlu0424@163.com>
2015-08-19 14:30:09 +0800

    Add PHP Design Registry

--- core/DbMongo.php
+++ core/DbMongo.php
@@ -4,13 +4,6 @@
  *
  * 用于mongodb数据库的增、删、改、查等操作
  *
- * @author tommy <streen003@gmail.com>
- * @copyright  Copyright (c) 2010 Tommy Software Studio
- * @link http://www.doitphp.com
- * @license New BSD License.{@link http://www.opensource.org/licenses/bsd-license.php}
- * @version $Id: MongoDb.class.php 2.0 2012-12-29 21:31:01Z tommy $
- * @package library
- * @since 1.0
  */
 class DbMongo {
 

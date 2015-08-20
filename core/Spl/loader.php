7b7e9358164bbbb02b51bf785b057b768c8966f5
xululu <xuyanlu0424@163.com>
2015-08-20 12:14:19 +0800

    add Proxy mode

--- core/Spl/loader.php
+++ core/Spl/loader.php
@@ -3,6 +3,8 @@
  * @var spl_autoload
  */
 class classAutolader {
+
+    private static $_extension = array();
     /**
      * [registerAutoload description]
      * @return [type] [description]
@@ -21,6 +23,16 @@ class classAutolader {
         include strtr($className, '_\\', '//') . '.class.php';
     }
 
+    /**
+     * [setExtension 注册并返回spl_autoload函数使用的默认文件扩展名]
+     * @param array $extension [description]
+     */
+    public static function setExtension ($extension = array()) {
+        if (empty($extension)) return false;
+        $_extension = implode(',', $extension);
+        // self::$_extension = $extension;
+        return spl_autoload_extensions($_extension);
+    }
 
     /**
      * [splClass 返回当前所有可用的 SPL 类的数组]
@@ -28,6 +40,11 @@ class classAutolader {
      */
     public static function splClass () {
         return spl_classes();
+    }
+
+
+    public static function splFunction () {
+        return spl_autoload_functions();
     }
 
 
@@ -39,8 +56,9 @@ $obj1 = new lib\A();
 
 $obj1->test();
 
-classAutolader::unregisterAutolad();
-$obj2 = new lib\B();
+
+// classAutolader::unregisterAutolad();
+// $obj2 = new lib\B();
 
 
 
